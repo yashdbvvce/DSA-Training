a0c035d190d7f76338fb57b338d28a86442e6e12
Vibhav <vibhavniranjan047579@gmail.com>
2025-02-07 09:57:46 +0530

    FirstQuestionCommit

--- src/Count_Of_Trailing_zeros_in_fact_Value.java
+++ src/Count_Of_Trailing_zeros_in_fact_Value.java
@@ -4,9 +4,16 @@ public class Count_Of_Trailing_zeros_in_fact_Value {
         // 10 -> 2
         // 15 -> 3
         // 20 -> 4
+        trailingZeros(150);
     }
 
     public static void trailingZeros(int n){
-
+        int power = 5;
+        int zeros = 0;
+        while(power <= n){
+            zeros  += n / power;
+            power = power * 5;
+        }
+        System.out.println("No of zeros = "+zeros);
     }
 }
