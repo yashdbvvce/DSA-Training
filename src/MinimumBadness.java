2739f5cfcbcf140ba193b89e7bb566a0c906b872
Yash Deepak Bambore <vvce22cse0119@vvce.ac.in>
2025-02-07 12:58:26 +0530

    Second Session Final

--- src/MinimumBadness.java
+++ src/MinimumBadness.java
@@ -1,27 +1,16 @@
 public class MinimumBadness {
     public static void main(String[] args) {
-        minimumBadness(new StringBuilder("RWBWBWR"));
+        minimumBadness(new StringBuilder("WWB"));
     }
 
     public static void minimumBadness(StringBuilder s){
         int badness = 0;
         for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == 'W'){
-               if(i-1 >= 0) s.setCharAt(i,s.charAt(i-1));
-               if(i-1 >= 0 && i+1 <= s.length()-1){
-                   if(s.charAt(i-1) != s.charAt(i+1)) badness++;
+           if(s.charAt(i) == 'W'){
+               if(s.charAt(i-1) != s.charAt(i+1)){
+                   s.setCharAt(i,s.charAt(i-1));
                }
-               else if(i+1 >=s.length()){
-                   if(s.charAt(i-1) != s.charAt(i)){
-                       badness++;
-                   }
-               }
-               else if(i - 1 < 0){
-                   if(s.charAt(i+1) != s.charAt(i)){
-                       badness++;
-                   }
-               }
-            }
+           }
         }
         System.out.println(s.toString());
         System.out.println(badness);
