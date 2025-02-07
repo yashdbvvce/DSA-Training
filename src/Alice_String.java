307d780508caa619c12d3e3863ee160037377c11
Yash Deepak Bambore <vvce22cse0119@vvce.ac.in>
2025-02-07 11:54:22 +0530

    Second Session

---
+++ src/Alice_String.java
@@ -0,0 +1,20 @@
+public class Alice_String {
+    public static void main(String[] args) {
+        System.out.println(LongestString(".........."));
+    }
+
+    public static int LongestString(String s){
+        int max = Integer.MIN_VALUE;
+        int currLength = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) != '.'){
+                currLength++;
+                max = Math.max(max,currLength);
+            }
+            else if(s.charAt(i) == '.'){
+                currLength = 0;
+            }
+        }
+        return Math.max(max,0);
+    }
+}
