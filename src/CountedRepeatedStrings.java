d6e19e8045f1a6bf8b312109eedd091660067eec
Yash Deepak Bambore <vvce22cse0119@vvce.ac.in>
2025-02-08 15:22:15 +0530

    Second day second session First Question - Minor Changes

--- src/CountedRepeatedStrings.java
+++ src/CountedRepeatedStrings.java
@@ -3,16 +3,17 @@ import java.util.ArrayList;
 public class CountedRepeatedStrings {
     public static void main(String[] args) {
         String s = "cat batman latt matter cat matter cat matter matter";
-        StringBuffer currString  = new StringBuffer("");
-        ArrayList<String> arr = new ArrayList<>();
-        for(int i = 0; i < s.length(); i++){
+    }
 
-            if(s.charAt(i) != ' '){
+    public static void getRepeatedWords(String s) {
+        StringBuffer currString = new StringBuffer("");
+        ArrayList<String> arr = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ') {
                 currString.append(s.charAt(i));
-            }
-            else{
-                if(s.substring(i,s.length()).contains(currString.toString())){
-                    if(!arr.contains(currString.toString())){
+            } else {
+                if (s.substring(i, s.length()).contains(currString.toString())) {
+                    if (!arr.contains(currString.toString())) {
                         arr.add(currString.toString());
                     }
                 }
