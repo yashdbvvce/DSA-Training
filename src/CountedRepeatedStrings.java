387220cda174c9c23e75be3ad26e8db3c78b7e99
Yash Deepak Bambore <vvce22cse0119@vvce.ac.in>
2025-02-08 14:48:50 +0530

    Second day second session First Question - Minor Changes

---
+++ src/CountedRepeatedStrings.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class CountedRepeatedStrings {
+    public static void main(String[] args) {
+        String s = "cat batman latt matter cat matter cat matter matter";
+        StringBuffer currString  = new StringBuffer("");
+        ArrayList<String> arr = new ArrayList<>();
+        for(int i = 0; i < s.length(); i++){
+
+            if(s.charAt(i) != ' '){
+                currString.append(s.charAt(i));
+            }
+            else{
+                if(s.substring(i,s.length()).contains(currString.toString())){
+                    if(!arr.contains(currString.toString())){
+                        arr.add(currString.toString());
+                    }
+                }
+                currString = new StringBuffer("");
+            }
+        }
+        System.out.println(arr);
+    }
+}
