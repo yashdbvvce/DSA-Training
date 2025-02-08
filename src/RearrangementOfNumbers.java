21e1884e8b84be555895f7829e49b0aef5985b3b
Yash Deepak Bambore <vvce22cse0119@vvce.ac.in>
2025-02-08 09:55:20 +0530

    Second day first session Second question

---
+++ src/RearrangementOfNumbers.java
@@ -0,0 +1,18 @@
+public class RearrangementOfNumbers {
+    public static void main(String[] args) {
+        System.out.println(rearrangmentOfBits(10));
+    }
+
+    public static int rearrangmentOfBits(int n){
+        int minimumValue = 0;
+        int currentPower = 0;
+        while(n > 0){
+            int rem = n % 2;
+            if(rem == 1){
+                minimumValue += (int)Math.pow(2,currentPower++);
+            }
+            n = n/2;
+        }
+        return minimumValue;
+    }
+}
